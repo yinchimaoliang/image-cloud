f140a23d93dfb4d1575d87d484aa861086416300
Yinhao Li <liyinhao0413@qq.com>
2019-05-30 19:26:06 +0800

    first commit

---
+++ MAIN.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import os
+
+
+DATASET_PATH = './data'
+
+class main():
+    def __init__(self):
+        self.dataset_path = DATASET_PATH
+
+    def getImages(self):
+        self.images =
